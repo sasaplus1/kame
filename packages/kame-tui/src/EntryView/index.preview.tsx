6cdc4bb8b1c0e39f2dabe152f8bb05cc868e6302
sasaplus1 <sasaplus1@gmail.com>
2023-09-18 00:15:28 +0900

    scrollable EntryView

--- packages/kame-tui/src/EntryView/index.preview.tsx
+++ packages/kame-tui/src/EntryView/index.preview.tsx
@@ -1,115 +1,142 @@
 #!/usr/bin/env node --no-warnings --loader tsx
 
+import type { Props as EntryViewProps } from './index';
+
 import * as React from 'react';
 import { render, useApp, useInput, Box, Text } from 'ink';
 import { EntryView } from './index';
 
+const entries: EntryViewProps['entries'] = [
+  {
+    entry: 'file.txt',
+    mode: '-rw-',
+    size: '100 kB',
+    date: '2023/09/17 00:05'
+  },
+  {
+    marker: '+',
+    entry: 'dir/',
+    mode: 'drwx',
+    size: '      ',
+    date: '2023/09/17 00:05'
+  },
+  {
+    entryStyles: { dimColor: true },
+    entry: '.gitignore',
+    mode: '-rw-',
+    size: '100 kB',
+    date: '2023/09/17 00:05'
+  },
+  {
+    entry: `t${'o'.repeat(512)}-long-file.txt`,
+    mode: '-rw-',
+    size: '100 kB',
+    date: '2023/09/17 00:05'
+  },
+  {
+    entry:
+      'いろはにほへとちりぬるをわかよたれそつねならむうゐのおくやまけふこえてあさきゆめみしゑひもせす.txt',
+    mode: '-rw-',
+    size: '100 kB',
+    date: '2023/09/17 00:05'
+  },
+  {
+    marker: '*',
+    entry: 'file.txt',
+    mode: '-rw-',
+    size: '100 kB',
+    date: '2023/09/17 00:05'
+  },
+  {
+    allStyles: { bold: true, dimColor: true },
+    entry: 'file.txt',
+    mode: '-rw-',
+    size: '100 kB',
+    date: '2023/09/17 00:05'
+  },
+  {
+    allStyles: { underline: true },
+    entry: 'file.txt',
+    mode: '-rw-',
+    size: '100 kB',
+    date: '2023/09/17 00:05'
+  },
+  {
+    allStyles: { inverse: true },
+    entry: 'file.txt',
+    mode: '-rw-',
+    size: '100 kB',
+    date: '2023/09/17 00:05'
+  },
+  {
+    cursorStyles: { backgroundColor: 'gray' },
+    markerStyles: { color: 'green' },
+    entryStyles: { color: 'red' },
+    modeStyles: { color: 'green' },
+    sizeStyles: { color: 'yellow' },
+    dateStyles: { color: 'blue' },
+    marker: '*',
+    entry: 'file.txt',
+    mode: '-rw-',
+    size: '100 kB',
+    date: '2023/09/17 00:05'
+  },
+  {
+    allStyles: { dimColor: true },
+    cursorStyles: { backgroundColor: 'gray' },
+    markerStyles: { color: 'green' },
+    entryStyles: { color: 'red' },
+    modeStyles: { color: 'green' },
+    sizeStyles: { color: 'yellow' },
+    dateStyles: { color: 'blue' },
+    marker: '*',
+    entry: 'file.txt',
+    mode: '-rw-',
+    size: '100 kB',
+    date: '2023/09/17 00:05'
+  }
+];
+
 function App() {
   const { exit } = useApp();
 
-  useInput((input) => {
+  const [offset, setOffset] = React.useState(Math.trunc(entries.length / 2));
+
+  useInput((input, key) => {
     if (input === 'q') {
       exit();
+    }
+
+    if (key.upArrow || input === 'k') {
+      if (offset <= 0) {
+        return;
+      }
+
+      setOffset((offset) => offset - 1);
+    }
+
+    if (key.downArrow || input === 'j') {
+      if (offset >= entries.length - 1) {
+        return;
+      }
+
+      setOffset((offset) => offset + 1);
     }
   });
 
   return (
     <React.Fragment>
-      <Text bold>press q to exit</Text>
+      <Text bold>
+        press q to exit | press up/down(or k/j) to change offset | offset:{' '}
+        {offset}
+      </Text>
       <Box flexDirection="column">
         <EntryView
+          height={Math.trunc(entries.length + 1 / 2) /* entries.length + 1 */}
           path="/home/sasaplus1"
           pathStyles={{ bold: true }}
-          entries={[
-            {
-              entry: 'file.txt',
-              mode: '-rw-',
-              size: '100 kB',
-              date: '2023/09/17 00:05'
-            },
-            {
-              marker: '+',
-              entry: 'dir/',
-              mode: 'drwx',
-              size: '      ',
-              date: '2023/09/17 00:05'
-            },
-            {
-              entryStyles: { dimColor: true },
-              entry: '.gitignore',
-              mode: '-rw-',
-              size: '100 kB',
-              date: '2023/09/17 00:05'
-            },
-            {
-              entry: `t${'o'.repeat(512)}-long-file.txt`,
-              mode: '-rw-',
-              size: '100 kB',
-              date: '2023/09/17 00:05'
-            },
-            {
-              entry:
-                'いろはにほへとちりぬるをわかよたれそつねならむうゐのおくやまけふこえてあさきゆめみしゑひもせす.txt',
-              mode: '-rw-',
-              size: '100 kB',
-              date: '2023/09/17 00:05'
-            },
-            {
-              marker: '*',
-              entry: 'file.txt',
-              mode: '-rw-',
-              size: '100 kB',
-              date: '2023/09/17 00:05'
-            },
-            {
-              allStyles: { bold: true, dimColor: true },
-              entry: 'file.txt',
-              mode: '-rw-',
-              size: '100 kB',
-              date: '2023/09/17 00:05'
-            },
-            {
-              allStyles: { underline: true },
-              entry: 'file.txt',
-              mode: '-rw-',
-              size: '100 kB',
-              date: '2023/09/17 00:05'
-            },
-            {
-              allStyles: { inverse: true },
-              entry: 'file.txt',
-              mode: '-rw-',
-              size: '100 kB',
-              date: '2023/09/17 00:05'
-            },
-            {
-              cursorStyles: { backgroundColor: 'gray' },
-              markerStyles: { color: 'green' },
-              entryStyles: { color: 'red' },
-              modeStyles: { color: 'green' },
-              sizeStyles: { color: 'yellow' },
-              dateStyles: { color: 'blue' },
-              marker: '*',
-              entry: 'file.txt',
-              mode: '-rw-',
-              size: '100 kB',
-              date: '2023/09/17 00:05'
-            },
-            {
-              allStyles: { dimColor: true },
-              cursorStyles: { backgroundColor: 'gray' },
-              markerStyles: { color: 'green' },
-              entryStyles: { color: 'red' },
-              modeStyles: { color: 'green' },
-              sizeStyles: { color: 'yellow' },
-              dateStyles: { color: 'blue' },
-              marker: '*',
-              entry: 'file.txt',
-              mode: '-rw-',
-              size: '100 kB',
-              date: '2023/09/17 00:05'
-            }
-          ]}
+          offset={offset}
+          entries={entries}
         />
       </Box>
     </React.Fragment>
--- packages/kame-tui/src/EntryView/index.tsx
+++ packages/kame-tui/src/EntryView/index.tsx
@@ -1,8 +1,10 @@
-import type { DOMElement, TextProps } from 'ink';
+import type { BoxProps, DOMElement, TextProps } from 'ink';
+import type { Props as ScrollBoxProps } from '@sasaplus1/ink-scroll-box';
 import type { Props as EntryProps } from '../Entry';
 
 import { measureElement, Box, Text } from 'ink';
 import * as React from 'react';
+import { ScrollBox } from '@sasaplus1/ink-scroll-box';
 import { Entry } from '../Entry';
 
 export type ColorProps = 'color' | 'backgroundColor' | 'dimColor';
@@ -18,7 +20,8 @@ export type Props = {
   entries: Omit<EntryProps, 'width'>[];
   path: string;
   pathStyles?: StyleProps;
-};
+} & Pick<ScrollBoxProps, 'offset'> &
+  BoxProps;
 
 export function EntryView(props: Props) {
   const ref = React.useRef<DOMElement>(null);
@@ -31,20 +34,22 @@ export function EntryView(props: Props) {
     }
   }, []);
 
-  const { entries, path, pathStyles } = props;
+  const { entries, path, pathStyles, offset, ...boxProps } = props;
 
   return (
-    <Box ref={ref} flexDirection="column">
+    <Box {...boxProps} ref={ref} flexDirection="column">
       <Text {...pathStyles}>{path}</Text>
-      {entries.map((entry, index) => (
-        <Entry
-          {...entry}
-          key={
-            path + entry.entry + entry.mode + entry.size + entry.date + index
-          }
-          width={width}
-        />
-      ))}
+      <ScrollBox offset={offset} height="100%">
+        {entries.map((entry, index) => (
+          <Entry
+            {...entry}
+            key={
+              path + entry.entry + entry.mode + entry.size + entry.date + index
+            }
+            width={width}
+          />
+        ))}
+      </ScrollBox>
     </Box>
   );
 }
